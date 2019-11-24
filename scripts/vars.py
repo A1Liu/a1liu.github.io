b7949d4ecb4cbf61f7303c8f555f14917b9cd276
Albert Liu <albertymliu@gmail.com>
2019-11-24 04:24:20 -0500

    updated scripts

--- scripts/vars.py
+++ scripts/vars.py
@@ -2,8 +2,8 @@ import os
 
 SCRIPTS_DIR = os.path.dirname(__file__)
 PROJECT_DIR = os.path.dirname(SCRIPTS_DIR)
-COLLECTIONS_DIR = os.path.join(PROJECT_DIR,'src','_collections')
+COLLECTIONS_DIR = os.path.join(PROJECT_DIR,'src')
 DRAFTS_DIR = os.path.join(COLLECTIONS_DIR,'_drafts')
 POSTS_DIR = os.path.join(COLLECTIONS_DIR,'_posts')
-ASSETS_DIR  = os.path.join(PROJECT_DIR,'assets')
+ASSETS_DIR  = os.path.join(PROJECT_DIR,'src','assets')
 BLOG_ASSETS_DIR = os.path.join(ASSETS_DIR,'blog')
